19f9ab41b4b48a56281c3bb4d77bdbd98ffb11af
richu <1691401076@qq.com>
2024-09-24 21:53:36 +0800

    chore:add react-redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,51 @@
-import React from "react";
+import { Outlet } from "react-router-dom"
+import { Badge, TabBar } from "antd-mobile"
+import {
+    AppOutline,
+    MessageOutline,
+    MessageFill,
+    UnorderedListOutline,
+    UserOutline,
+} from 'antd-mobile-icons'
+export default function CityList() {
+    const tabs = [
+        {
+            key: 'home',
+            title: '首页',
+            icon: <AppOutline />,
+            badge: Badge.dot,
+        },
+        {
+            key: 'todo',
+            title: '待办',
+            icon: <UnorderedListOutline />,
+            badge: '5',
+        },
+        {
+            key: 'message',
+            title: '消息',
+            icon: (active) =>
+                active ? <MessageFill /> : <MessageOutline />,
+            badge: '99+',
+        },
+        {
+            key: 'personalCenter',
+            title: '我的',
+            icon: <UserOutline />,
+        },
+    ]
 
-export default class CityList extends React.Component{
-    render(){
-        return <div>首页</div>
-    }
+    return (
+        <div>
+            <span>首页</span>
+            <Outlet></Outlet>
+            <TabBar style={{width:"390px",position: "absolute",bottom:"0",borderTop:"solid 1px blue",borderBottom:"solid 1px blue"}}>
+                {tabs.map(item => (
+                    <TabBar.Item key={item.key} icon={item.icon} title={item.title} badge={item.badge} 
+                    
+                    />
+                ))}
+            </TabBar>
+        </div>
+    )
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from '../pages/Home'
 import CityList from '../pages/CityList'
-
+import News from "../pages/News";
 
 const router = createBrowserRouter([
     {
-        index:true,
-        element:<Home/>
+        path:"/",
+        element:<Home/>,
+        children:[
+            {
+                path:"/news",
+                element:<News></News>,
+                title:"新闻页"
+            }
+        ]
     },
     {
         path:"/cityList",
