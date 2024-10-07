078e1561e43e3d6cbf68c61008152e87862182a8
richu <1691401076@qq.com>
2024-10-07 20:37:50 +0800

    chore:update the widgets of CityLists

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {RouterProvider} from 'react-router-dom'
 import router from './routes'
 import { Provider } from 'react-redux';
 import store from './stores';
+import 'react-virtualized/styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -1,21 +1,23 @@
-import { NavBar,Space } from "antd-mobile"
-import {SearchOutline,LeftOutline} from 'antd-mobile-icons'
+import { NavBar, Space } from "antd-mobile"
+import { SearchOutline, LeftOutline } from 'antd-mobile-icons'
 import { useNavigate } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { fetchCityList } from "../../stores/City"
 import { useDispatch, useSelector } from "react-redux"
 import axios from "axios"
 import { getCurrentCity } from "../../utils"
+import { AutoSizer, List } from "react-virtualized"
+import './index.scss'
 
-const formatData = (list)=>{
+const formatData = (list) => {
   const cityList = []
   let cityIndex = []
 
   list.forEach(ele => {
-    const first = ele.short.substr(0,1)
-    if (cityList[first]){
+    const first = ele.short.substr(0, 1)
+    if (cityList[first]) {
       cityList[first].push(ele)
-    }else{
+    } else {
       cityList[first] = [ele]
       cityIndex.push(first)
     }
@@ -27,44 +29,84 @@ const formatData = (list)=>{
   }
 }
 
-export default function CityList() {
-  const [hotList,setHotList] = useState([])
-  const [city,setCity] = useState("")
+export default function CityLists() {
+  const [hotList, setHotList] = useState([])
+  const [city, setCity] = useState("")
   const dispatch = useDispatch()
-    useEffect(()=>{
-      dispatch(fetchCityList())
-      const fetchHotCity = async()=>{
-        const res = await axios.get(`http://localhost:8080/area/hot`)
-        setHotList(res.data.body)
-      }
-      fetchHotCity()
-      getCurrentCity().then((value)=>{
-        setCity(value)
-      })
-    },[dispatch,setCity])
+  const { CList } = useSelector(state => state.CityList)
+  const { cityIndex, cityList } = formatData(CList)
+  useEffect(() => {
+    dispatch(fetchCityList())
+    const fetchHotCity = async () => {
+      const res = await axios.get(`http://localhost:8080/area/hot`)
+      setHotList(res.data.body)
+    }
+    fetchHotCity()
+    getCurrentCity().then((value) => {
+      setCity(value)
+    })
+    
+  }, [dispatch, setCity])
+  cityIndex.unshift('hot')
+  cityIndex.unshift('#')
+  cityList['hot'] = hotList
+  cityList['#'] = [{"label":city}]
+  const navigate = useNavigate()
+  const right = (
+    <div style={{ fontSize: 20 }}>
+      <Space style={{ '--gap': '16px' }}>
+        <SearchOutline style={{ marginRight: "30px" }} />
+      </Space>
+    </div>
+  )
+  
+  const handleIndex = (letter) => {
+    switch (letter) {
+      case "#":
+        return "当前定位"
+      case "hot":
+        return "热门城市"
+      default:
+        return letter.toUpperCase()
+    }
+  }
 
-    const {CList} = useSelector(state=>state.CityList)
-    const {cityIndex,cityList} = formatData(CList)
-    cityIndex.unshift('hot')
-    cityIndex.unshift('#')
-    cityList['hot'] = hotList
-    cityIndex['#'] = [city]
-    const navigate = useNavigate()
-    const right = (
-      <div style={{ fontSize: 20 }}>
-        <Space style={{ '--gap': '16px' }}>
-          <SearchOutline style={{marginRight:"30px"}} />
-        </Space>
-      </div>
-    )
-    return (
-      <div>
-        <NavBar right={right} backIcon={<LeftOutline style={{fontSize:"20"}} onClick={()=>navigate(-1)}/>}>
-          城市列表
-        </NavBar>
-        {/* 可视区域渲染  react-virtualized*/}
-        <div>当前位置{city}</div>
-      </div>
-    )
+  return (
+    <div className="citylist">
+      <NavBar style={{ marginTop: "-45px" }} right={right} backIcon={<LeftOutline style={{ fontSize: "20" }} onClick={() => navigate(-1)} />}>
+        城市列表
+      </NavBar>
+      {/* 可视区域渲染  react-virtualized*/}
+
+      <AutoSizer>
+        {({ height, width }) => (
+          <List
+            // 窗口的高度,必填
+            height={height}
+            // 窗口的宽度,必填
+            width={width}
+            // 总共个数
+            rowCount={cityIndex.length}
+            // cell高度
+            rowHeight={50}
+            style={{ outline: "none", }}
+            rowRenderer={({ key, index, isScrolling, style }) => {
+              const letter = cityIndex[index]
+              return (<div key={key} style={style} className="cityList">
+                <div className="index" key={key}>{handleIndex(letter)}</div>
+                <div className="city">{cityList[letter] == null ? <div>loading</div> : 
+                  cityList[letter].map(item => <div className="name" key={item.value}>
+                    {item.label}
+                  </div>
+                  )
+                }</div>
+              </div>)
+            }}>
+          </List>
+        )}
+      </AutoSizer>
+
+    </div>
+  )
 
 }
